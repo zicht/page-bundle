b48f55fdb6236a84cb843c4deb21f07198cedf0c
Gerard van Helden <gerard@zicht.nl>
2013-11-11 10:59:31 +0000

    replaced yml by xml services config; made aliasing strategy class configurable
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtPageBundle/trunk@27866 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/PageBundle/DependencyInjection/ZichtPageExtension.php
+++ src/Zicht/Bundle/PageBundle/DependencyInjection/ZichtPageExtension.php
@@ -26,8 +26,8 @@ class ZichtPageExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
 
         $container
             ->getDefinition('zicht_page.form.type.zicht_content_item_region_type')
