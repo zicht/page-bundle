49c034173a78877c67ac37f8a8abf4423177eea6
Gerard van Helden <gerard@zicht.nl>
2014-07-31 16:52:34 +0000

    fixes the ordering of bundles issue
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtPageBundle/trunk@42631 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/PageBundle/DependencyInjection/CompilerPass/GenerateAdminServicesCompilerPass.php
+++ src/Zicht/Bundle/PageBundle/DependencyInjection/CompilerPass/GenerateAdminServicesCompilerPass.php
@@ -9,7 +9,7 @@ namespace Zicht\Bundle\PageBundle\DependencyInjection\CompilerPass;
 use \Symfony\Component\DependencyInjection\ContainerBuilder;
 use \Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use \Symfony\Component\DependencyInjection\Reference;
-use Zicht\Bundle\PageBundle\Model\ContentItemContainer;
+use \Zicht\Bundle\PageBundle\Model\ContentItemContainer;
 use \Zicht\Util\Str;
 
 /**
@@ -22,6 +22,11 @@ class GenerateAdminServicesCompilerPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
+        $config = $container->getParameter('zicht_page.config');
+        if (empty($config['admin'])) {
+            return;
+        }
+
         $naming = function($fqEntityClassName) {
             return
                 str_replace(
@@ -31,76 +36,74 @@ class GenerateAdminServicesCompilerPass implements CompilerPassInterface
                 ) . 'Admin';
         };
 
-
         $config = $container->getParameter('zicht_page.config');
-        if (!empty($config['admin'])) {
-            $baseIds = $config['admin']['base'];
 
-            $serviceDefs = array();
-            $pageManagerDef = $container->getDefinition('zicht_page.page_manager');
-            $types = array();
+        $baseIds = $config['admin']['base'];
 
-            foreach ($pageManagerDef->getMethodCalls() as $call) {
-                switch ($call[0]) {
-                    case 'setPageTypes':
-                        $types['page'] = $call[1][0];
-                        break;
-                    case 'setContentItemTypes':
-                        $types['contentItem'] = $call[1][0];
-                        break;
+        $serviceDefs = array();
+        $pageManagerDef = $container->getDefinition('zicht_page.page_manager');
+        $types = array();
+
+        foreach ($pageManagerDef->getMethodCalls() as $call) {
+            switch ($call[0]) {
+                case 'setPageTypes':
+                    $types['page'] = $call[1][0];
+                    break;
+                case 'setContentItemTypes':
+                    $types['contentItem'] = $call[1][0];
+                    break;
+            }
+        }
+
+        foreach (array('page', 'contentItem') as $type) {
+            $serviceDefs[$type] = array();
+
+            $def = $container->getDefinition($baseIds[$type]);
+
+            foreach ($types[$type] as $entityClassName) {
+                $adminClassName = $naming($entityClassName);
+                if (class_exists($adminClassName)) {
+                    $adminService = clone $def;
+                    $adminService->setClass($adminClassName);
+
+                    $tags = $adminService->getTags();
+                    $tags['sonata.admin'][0]['show_in_dashboard'] = 0;
+                    $tags['sonata.admin'][0]['label'] = Str::rstrip(Str::classname($entityClassName), 'Page');
+                    $adminService->setTags($tags);
+
+                    $id = $baseIds[$type]. '.' . Str::uscore(Str::classname($entityClassName));
+
+                    $adminService->replaceArgument(0, $id);
+                    $adminService->replaceArgument(1, $entityClassName);
+
+                    $container->setDefinition($id, $adminService);
+
+                    $serviceDefs[$type][]= $id;
                 }
             }
+        }
 
-            foreach (array('page', 'contentItem') as $type) {
-                $serviceDefs[$type] = array();
+        foreach ($serviceDefs['page'] as $pageServiceId) {
+            $pageDef = $container->getDefinition($pageServiceId);
+            $pageClassName = $pageDef->getArgument(1);
 
-                $def = $container->getDefinition($baseIds[$type]);
+            if (is_a($pageClassName, 'Zicht\Bundle\PageBundle\Model\ContentItemContainer', true)) {
 
-                foreach ($types[$type] as $entityClassName) {
-                    $adminClassName = $naming($entityClassName);
-                    if (class_exists($adminClassName)) {
-                        $adminService = clone $def;
-                        $adminService->setClass($adminClassName);
+                /** @var ContentItemContainer $instance */
+                $instance = new $pageClassName;
 
-                        $tags = $adminService->getTags();
-                        $tags['sonata.admin'][0]['show_in_dashboard'] = 0;
-                        $tags['sonata.admin'][0]['label'] = Str::rstrip(Str::classname($entityClassName), 'Page');
-                        $adminService->setTags($tags);
+                if ($matrix = $instance->getContentItemMatrix()) {
+                    $contentItemClassNames = $matrix->getTypes();
 
-                        $id = $baseIds[$type]. '.' . Str::uscore(Str::classname($entityClassName));
+                    foreach ($serviceDefs['contentItem'] as $contentItemServiceId) {
+                        $contentItemDefinition = $container->getDefinition($contentItemServiceId);
 
-                        $adminService->replaceArgument(0, $id);
-                        $adminService->replaceArgument(1, $entityClassName);
-
-                        $container->setDefinition($id, $adminService);
-
-                        $serviceDefs[$type][]= $id;
-                    }
-                }
-            }
-
-            foreach ($serviceDefs['page'] as $pageServiceId) {
-                $pageDef = $container->getDefinition($pageServiceId);
-                $pageClassName = $pageDef->getArgument(1);
-
-                if (is_a($pageClassName, 'Zicht\Bundle\PageBundle\Model\ContentItemContainer', true)) {
-
-                    /** @var ContentItemContainer $instance */
-                    $instance = new $pageClassName;
-
-                    if ($matrix = $instance->getContentItemMatrix()) {
-                        $contentItemClassNames = $matrix->getTypes();
-
-                        foreach ($serviceDefs['contentItem'] as $contentItemServiceId) {
-                            $contentItemDefinition = $container->getDefinition($contentItemServiceId);
-
-                            if (in_array($contentItemDefinition->getArgument(1), $contentItemClassNames)) {
-                                $pageDef
-                                    ->addMethodCall(
-                                        'addChild',
-                                        array(new Reference($contentItemServiceId))
-                                    );
-                            }
+                        if (in_array($contentItemDefinition->getArgument(1), $contentItemClassNames)) {
+                            $pageDef
+                                ->addMethodCall(
+                                    'addChild',
+                                    array(new Reference($contentItemServiceId))
+                                );
                         }
                     }
                 }
--- src/Zicht/Bundle/PageBundle/ZichtPageBundle.php
+++ src/Zicht/Bundle/PageBundle/ZichtPageBundle.php
@@ -8,6 +8,7 @@ namespace Zicht\Bundle\PageBundle;
 use \Symfony\Component\DependencyInjection\ContainerBuilder;
 use \Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use \Symfony\Component\HttpKernel\Bundle\Bundle;
+use \Sonata\AdminBundle\DependencyInjection\Compiler\AddDependencyCallsCompilerPass;
 
 /**
  * Bundle providing "page" -> "content-item" structure
@@ -21,9 +22,25 @@ class ZichtPageBundle extends Bundle
     {
         parent::build($container);
 
-        $container->addCompilerPass(
-            new DependencyInjection\CompilerPass\GenerateAdminServicesCompilerPass(),
-            PassConfig::TYPE_BEFORE_OPTIMIZATION
-        );
+        $generatorPass = new DependencyInjection\CompilerPass\GenerateAdminServicesCompilerPass();
+
+        // if the Sonata bundle is configured before this one, the compilerpass must be injected right before,
+        // otherwise the generated services will not be recognized by Sonata.
+        $idx = null;
+        $beforeOptimizationPasses = $container->getCompilerPassConfig()->getBeforeOptimizationPasses();
+
+        foreach ($beforeOptimizationPasses as $i => $pass) {
+            if ($pass instanceof AddDependencyCallsCompilerPass) {
+                $idx = $i;
+                break;
+            }
+        }
+
+        if (null !== $idx) {
+            array_splice($beforeOptimizationPasses, $idx, 0, array($generatorPass));
+        } else {
+            $beforeOptimizationPasses[]= $generatorPass;
+        }
+        $container->getCompilerPassConfig()->setBeforeOptimizationPasses($beforeOptimizationPasses);
     }
 }
